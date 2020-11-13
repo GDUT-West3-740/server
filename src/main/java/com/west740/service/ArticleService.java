f2617c820f8b2c1544a68ae2001728bfac6604a6
Lucifer <zoew1942spe@live.com>
2020-11-14 01:04:47 +0800

    feat(west740):修复了部分bug

--- src/main/java/com/west740/service/ArticleService.java
+++ src/main/java/com/west740/service/ArticleService.java
@@ -42,23 +42,45 @@ public class ArticleService {
             String stripHtml = stripHtml(article.getHtmlContent());
             article.setSummary(stripHtml.substring(0,stripHtml.length()>50?50:stripHtml.length()));
         }
-        if (article.getState()==-1){
-            article.setUid(Util.getCurrentUser().getId());
-        }
-        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-        if (article.getState() == 1) {
-            article.setPublishDate(timestamp);
-        }
-        article.setEditTime(new Timestamp(System.currentTimeMillis()));
-        int i = articleMapper.updateArticle(article);
-        String[] dynamicTags = article.getDynamicTags();
-        if (dynamicTags != null && dynamicTags.length > 0) {
-            int tags = addTagsToArticle(dynamicTags, article.getId());
-            if (tags == -1) {
-                return tags;
+        if (article.getId() == -1) {
+            //添加操作
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+            if (article.getState() == 1) {
+                //设置发表日期
+                article.setPublishDate(timestamp);
             }
+            article.setEditTime(timestamp);
+            //设置当前用户
+            article.setUid(Util.getCurrentUser().getId());
+            int i = articleMapper.addNewArticle(article);
+            //打标签
+            String[] dynamicTags = article.getDynamicTags();
+            if (dynamicTags != null && dynamicTags.length > 0) {
+                int tags = addTagsToArticle(dynamicTags, article.getId());
+                if (tags == -1) {
+                    return tags;
+                }
+            }
+            return i;
+        } else {
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+            if (article.getState() == 1) {
+                //设置发表日期
+                article.setPublishDate(timestamp);
+            }
+            //更新
+            article.setEditTime(new Timestamp(System.currentTimeMillis()));
+            int i = articleMapper.updateArticle(article);
+            //修改标签
+            String[] dynamicTags = article.getDynamicTags();
+            if (dynamicTags != null && dynamicTags.length > 0) {
+                int tags = addTagsToArticle(dynamicTags, article.getId());
+                if (tags == -1) {
+                    return tags;
+                }
+            }
+            return i;
         }
-        return i;
     }
 
     private int addTagsToArticle(String[] dynamicTags, Long aid) {
